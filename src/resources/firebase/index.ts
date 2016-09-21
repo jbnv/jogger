00eaafc9f229bfde152ad841a5643b631d28cd5d
Jay Bienvenu <bienvenunet@yahoo.com>
2016-09-21 13:23:53 -0500

    Add keys to raw items.

--- src/resources/firebase/index.ts
+++ src/resources/firebase/index.ts
@@ -143,9 +143,15 @@ export class FirebaseCollectionModule extends FirebaseModule {
 
   // returns items as a filtered and sorted array
   get items() {
-    //console.log("JogIndex.items");
     let rawItems = this.collection.items;
-    let subset = Object.keys(rawItems).map(key => rawItems[key]);
+    let subset = [];
+    for (var key in rawItems) {
+      if (key[0] === "-") {
+        var item = rawItems[key];
+        item.key = key;
+        subset.push(item);
+      }
+    }
     // Now items is an array, and we can sort and filter it for delivery.
     // this.filters.forEach(function(filter) {
     //   items = items.filter(item.check,this);
