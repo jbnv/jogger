9174ee4149f4d9eb508f1878114553a7751226b8
Jay Bienvenu <toptal@bienv.com>
2016-09-24 08:48:00 -0500

    Show admin/manager status in identity.

--- src/resources/elements/identity.ts
+++ src/resources/elements/identity.ts
@@ -1,6 +1,7 @@
 import {customElement} from 'aurelia-framework';
 import {FirebaseModule, inject, AuthenticationManager, Router} from '../firebase/index';
 import {EventAggregator} from 'aurelia-event-aggregator';
+import {currentUserProperties} from '../user';
 
 @inject(AuthenticationManager, Router,EventAggregator)
 @customElement('identity')
@@ -8,6 +9,7 @@ export class Identity extends FirebaseModule {
 
   user = null;
   email = null;
+  icon = null;
 
   constructor(
     authManager:AuthenticationManager,
@@ -18,6 +20,13 @@ export class Identity extends FirebaseModule {
     eventAggregator.subscribe('user-signin',(user) => {
       this.user = user;
       this.email = user.email;
+      let properties = currentUserProperties();
+    });
+    eventAggregator.subscribe('user-properties',(properties) => {
+      console.log("user-properties: properties",properties);
+      console.log("user-properties: properties.icon",properties.icon);
+      this.icon = properties.icon;
+      console.log("user-properties: icon",this.icon);
     });
   }
 
@@ -25,6 +34,7 @@ export class Identity extends FirebaseModule {
     this.authManager.signOut().then(() => {
       this.user = null;
       this.email = null;
+      this.icon = null;
       this.router.navigateToRoute('home');
     });
   }
