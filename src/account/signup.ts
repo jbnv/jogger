b95debc7fb25b15afccbffba53d97e1d81bbbfc1
Jay Bienvenu <toptal@bienv.com>
2016-09-24 10:22:38 -0500

    Create a user profile entity when registering a new user.

--- src/account/signup.ts
+++ src/account/signup.ts
@@ -1,4 +1,6 @@
+import * as Firebase from 'firebase';
 import {FirebaseModule} from '../resources/firebase/index';
+import {User} from '../entities/user';
 
 export class SignUp extends FirebaseModule {
   email = null;
@@ -8,7 +10,13 @@ export class SignUp extends FirebaseModule {
     console.log("SignUp.signUp");
     this.clearState();
     this.authManager.createUserAndSignIn(this.email, this.password)
-      .then(() => {
+      .then((fbUser) => {
+        //Create user properties.
+        let user = new User();
+        user.ownerId = fbUser.uid;
+        user.role = 'user';
+        //++ This is ugly and should be encapsulated.
+        Firebase.database().ref(`users/${user.ownerId}`).set(user.clone());
         this.router.navigateToRoute('accountIndex');
       })
       .catch((e) => {
