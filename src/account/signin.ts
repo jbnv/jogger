f7d3324dfe0807a2f7f4f6e41e7db97e6c259cc4
Jay Bienvenu <toptal@bienv.com>
2016-09-25 10:53:17 -0500

    Add ability to disable users. Handle disabled users.

--- src/account/signin.ts
+++ src/account/signin.ts
@@ -1,4 +1,5 @@
-import {FirebaseModule} from '../resources/firebase/index';
+import * as Firebase from 'firebase';
+import {FirebaseModule,ReactiveCollection} from '../resources/firebase/index';
 
 export class SignIn extends FirebaseModule {
   email = null;
@@ -7,11 +8,24 @@ export class SignIn extends FirebaseModule {
   signIn() {
     this.clearState();
     this.authManager.signIn(this.email, this.password)
-      .then(() => {
+    .then((fbUser) => {
+      //++ This is ugly and should be encapsulated.
+      Firebase.database().ref(`users/${fbUser.uid}/role`).once('value')
+      .then((snapshot) => {
+        if (snapshot.val() == "disabled") {
+          throw new Error("Your account is disabled.");
+        }
         this.router.navigateToRoute('jogIndex');
+        return true;
       })
       .catch((e) => {
-        this.setStateError(e.message);
+        this.setStateError(e.message || "Unable to verify user account.");
+        this.authManager.signOut();
       });
+    })
+    .catch((e) => {
+      this.setStateError(e.message || "Unable to verify user account.");
+      this.authManager.signOut();
+    });
   }
 }
