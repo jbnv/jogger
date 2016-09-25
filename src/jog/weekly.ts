4cdb6922d347c6bba1be02b77f5b8dedc4f3d3e8
Jay Bienvenu <toptal@bienv.com>
2016-09-25 10:51:06 -0500

    Weekly: Fix bugs and improve formatting.

--- src/jog/weekly.ts
+++ src/jog/weekly.ts
@@ -10,7 +10,6 @@ import {sortByDate} from '../transform';
 export class JogWeekly extends FirebaseCollectionModule {
 
   collection: JogCollection;
-  reducedData = {};
   reducedDataAsArray = [];
   user = null;
   dateFormat = 'YYYY-MM-DD';
@@ -23,31 +22,32 @@ export class JogWeekly extends FirebaseCollectionModule {
   ) {
     super(authManager,router,eventAggregator);
     this.collection = collection;
-    this.sortFn = sortByDate;
 
     // Reduce and group by weeks.
     let refMoment = moment('2000-01-01');
+    let reducedData = {};
     for (var key in this.collection.items) {
+      if (key == '__firebaseKey__') continue;
       let item = this.collection.items[key];
       let currentMoment = moment(item.date);
       let week = currentMoment.diff(refMoment,'weeks');
-      if (!this.reducedData[week]) {
-        this.reducedData[week] = {
+      if (!reducedData[week]) {
+        reducedData[week] = {
           startDate: refMoment.clone().add(week,'weeks').format(this.dateFormat),
           endDate: refMoment.clone().add(week,'weeks').add(6,'days').format(this.dateFormat),
           jogs: 0,
           totalDistance: 0.0,
           totalTime: 0.0
         };
-        //WARNING: Does not yet account for possible differences in units of measure!
-        this.reducedData[week].jogs++;
-        this.reducedData[week].totalDistance += parseFloat(item.distance);
-        this.reducedData[week].totalTime += parseFloat(item.time);
       }
+      //WARNING: Does not yet account for possible differences in units of measure!
+      reducedData[week].jogs++;
+      reducedData[week].totalDistance += parseFloat(item.distance);
+      reducedData[week].totalTime += parseFloat(item.time);
     }
 
-    Object.keys(this.reducedData).sort().forEach(key => {
-      this.reducedDataAsArray.push(this.reducedData[key]);
+    Object.keys(reducedData).sort().forEach(key => {
+      this.reducedDataAsArray.push(reducedData[key]);
     });
   }
 
