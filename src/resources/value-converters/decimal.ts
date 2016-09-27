6c2ccacd99e8ce7b88b3443e8b267d3517ab4a48
Jay Bienvenu <toptal@bienv.com>
2016-09-27 08:46:58 -0500

    Fix formatting.

--- src/resources/value-converters/decimal.ts
+++ src/resources/value-converters/decimal.ts
@@ -3,6 +3,8 @@ import numeral from 'numeral';
 export class DecimalValueConverter {
   toView(value,places) {
     //return numeral(value).format('0.0000'.substring(0,places+2));
-    return value;
+    //return value;
+    let modulus = Math.pow(10,places);
+    return Math.floor(value*modulus)/modulus;
   }
 }
